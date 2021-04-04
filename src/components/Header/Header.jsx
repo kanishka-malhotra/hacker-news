bb6f817cdf9a2b9114e9a4beca11ac07337ab685
Kanishka Malhotra <kanishkamalhotra28@gmail.com>
2021-04-04 23:48:15 +0530

    Create folder structure and remove extra files/comments from create-react-app initial scaffold

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/logo.svg';
+
+const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    Hacker News (Reloaded)
+  </header>
+);
+
+export default Header;
